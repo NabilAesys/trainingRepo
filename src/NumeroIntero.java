ea18d8865bed9ac097e94dc5cbd7121f28362508
nabilAesys <nabil.ezzaki@aesys.tech>
2024-03-07 16:36:18 +0100

    added control with try catch and fixed

--- src/NumeroIntero.java
+++ src/NumeroIntero.java
@@ -1,39 +1,35 @@
-
+import java.util.Scanner;
 
 public class NumeroIntero {
-
-    private int num;
+    private  int num;
     int somma = 0;
 
     public NumeroIntero(int num) {
         this.num = num;
     }
-
-    public int getNum() {
-        return num;
+    public NumeroIntero() {
     }
 
-    public void setNum(int num) {
-        this.num = num;
-    }
+    public void start(){
+        try {
+            System.out.println("--------INTEGER SCANNER INPUT-------");
+            Scanner scanner1 = new Scanner(System.in);
+            System.out.println("Inserisci un numero");
+            int inputUtente1 = scanner1.nextInt();
+            NumeroIntero numeroIntero = new NumeroIntero(inputUtente1);
+            int i = numeroIntero.sommaCifreNumeroIntero();
+            System.out.println("la somma delle cifre presenti nel numero intero è: " + i);
+        }catch (Exception e){
+            System.out.println("errore durante l'inserimento");
+        }
 
-    @Override
-    public String toString() {
-        return "NumeroIntero{" +
-                "num=" + num +
-                ", somma=" + somma +
-                '}';
     }
 
     public  int sommaCifreNumeroIntero() {
-
-        // Finché il numero non diventa 0, prendo l'ultima cifra, la sommo alla somma totale e divido il numero per 10
         while (num != 0) {
             somma += num % 10;
             num /= 10;
         }
         return somma;
     }
-
 }
-
--- src/SommaNum.java
+++ src/SommaNum.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 public class SommaNum {
-
+    public SommaNum() {
+    }
     private String numeri;
-
+    int somma = 0;
     public SommaNum(String numeri) {
         this.numeri = numeri;
     }
 
-    public int sommaCifre() {
-        int somma = 0;
+    public void start(){
+        try {
+            System.out.println("--------STRING SCANNER INPUT-------");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Inserisci un numero");
+            String numeri = scanner.nextLine();
+            SommaNum sommaNum = new SommaNum(numeri);
+            int somma = sommaNum.sommaCifre();
+            System.out.println("la somma delle cifre presenti nella stringa è: " + somma);
+        }catch (Exception e){
+            System.out.println("errore durante l'inserimento");
+        }
 
-        // Itera attraverso i caratteri della stringa
+    }
+    public int sommaCifre() {
         for (int i = 0; i < numeri.length(); i++) {
             // Converte il carattere corrente in un numero intero e lo somma alla somma totale
             somma += Character.getNumericValue(numeri.charAt(i));
