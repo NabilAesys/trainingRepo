23108e7ff29b0f28a88d95672188801d7e47b691
nabilAesys <nabil.ezzaki@aesys.tech>
2024-03-07 14:16:46 +0100

    cleansing code of Master

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,26 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("--------STRING SCANNER INPUT-------");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Inserisci un numero");
-        String numeri = scanner.nextLine();  //
-        SommaNum sommaNum = new SommaNum(numeri);
-        int somma = sommaNum.sommaCifre();
 
 
 
-        System.out.println("--------INTEGER SCANNER INPUT-------");
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.println("Inserisci un numero");
-        Integer inputUtente1 = scanner1.nextInt();
-        NumeroIntero numeroIntero = new NumeroIntero(inputUtente1);
-        int i = numeroIntero.sommaCifreNumeroIntero();
 
 
-
-        System.out.println("la somma delle cifre presenti nel numero intero è: " + i);
-        System.out.println("la somma delle cifre presenti nella stringa è: " + somma);
 
 
 
